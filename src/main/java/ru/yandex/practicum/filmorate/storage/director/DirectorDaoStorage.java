c1ebff9aa33c516e41055e2f8aa0f5dc350ffbd7
Sergey <Melkii914@mail.ru>
2023-05-25 13:00:05 +0300

    fix: fixes5

--- src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDaoStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDaoStorage.java
@@ -7,10 +7,15 @@ import java.util.List;
 public interface DirectorDaoStorage {
 
     List<Director> getAll();
+
     Director createDirector(Director director);
+
     void deleteDirector(Long id);
+
     Director updateDirector(Director director);
+
     Director getDirector(Long id);
+
     List<Long> findFilmsByDirector(Long directorId, String sortBy);
 
 }
