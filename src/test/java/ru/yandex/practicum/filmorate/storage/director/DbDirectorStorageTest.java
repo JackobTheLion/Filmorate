b6738dd6fb4f5df0d0d4888806c74697f46ecc58
Sergey <Melkii914@mail.ru>
2023-05-25 13:20:31 +0300

    fix: fixes7

--- src/test/java/ru/yandex/practicum/filmorate/storage/director/DbDirectorStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/director/DbDirectorStorageTest.java
@@ -15,7 +15,6 @@ import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
