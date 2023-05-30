2469f227a0c76d9b46dd668ddae873f230e0621b
Olga <110121612+Olik1@users.noreply.github.com>
2023-05-30 21:15:03 +0300

    changes
    style

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/DbUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/DbUserStorage.java
@@ -14,9 +14,7 @@ import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Component
 @Qualifier("dbStorage")
